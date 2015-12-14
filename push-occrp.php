f03c4f9c30dfe168c8dbe71fffef5bc034334fb9
Christopher Guess <christopher@Christophers-MacBook-Pro.local>
2015-12-14 23:59:34 +0100

    initial commit

---
+++ push-occrp.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Plugin Name: Push Occrp
+ * Plugin URI:  https://vccw.cc/
+ * Description: An export plugin for the Push-OCCRP mobile app ecosystem.
+ * Version:	 0.1
+ * Author:	  Christopher Guess
+ * Author URI:  https://www.tryandguess.com/
+ * License:	 
+ */
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+add_filter( 'rewrite_rules_array','my_insert_rewrite_rules' );
+add_filter( 'query_vars','my_insert_query_vars' );
+add_action( 'wp_loaded','my_flush_rules' );
+add_action( 'template_redirect', 'push_endpoint_data');
+
+// flush_rules() if our rules are not yet included
+function my_flush_rules(){
+    $rules = get_option( 'rewrite_rules' );
+
+    if ( ! isset( $rules['push-occrp/(.*?)/(.+?)'] ) ) {
+        global $wp_rewrite;
+        $wp_rewrite->flush_rules();
+    }
+}
+
+// Adding a new rule
+function my_insert_rewrite_rules( $rules )
+{
+    $newrules = array();
+    $newrules['push-occrp/([0-9a-zA-Z]+)/([0-9a-zA-Z]+)/'] = 'index.php?push=$matches[1]&myargument=$matches[2]';
+    return $newrules + $rules;
+}
+
+// Adding the id var so that WP recognizes it
+function my_insert_query_vars( $vars )
+{
+    array_push($vars, 'push', 'myargument');
+    return $vars;
+}
+
+function push_endpoint_data() {
+	$query = get_query_var("push");
+	
+	if(!$query) {
+		return;
+	}
+ 
+    $response = array();
+    $response['start_date'] = null;
+    $response['end_date'] = null;
+    $response['total_items'] = 0;
+    $response['total_pages'] = 1;
+    $response['page'] = 1;
+    $response['results'] = array();
+    
+    $args = array(
+        'post_type'      => 'post',
+        'posts_per_page' => 10,
+    );
+ 
+    $post_query = new WP_Query( $args );
+ 
+    if ( $post_query->have_posts() ) {
+		while ( $post_query->have_posts() ) {
+			$post_query->the_post();
+			$post_data = array();
+            
+			$post_data["headline"] = get_the_title();
+			$post_data["description"] = get_the_excerpt();
+			$post_data["body"] = get_the_content();
+			$post_data["author"] = get_the_author();
+			$post_data["publish_date"] = get_the_date("Ymd");
+			$post_data["id"] = get_the_id();
+            $post_data["language"] = "en-GB";
+            $post_data["image_urls"] = array();
+            $post_data["captions"] = array(); 
+            
+            $response['total_items']++;
+            
+            $response['results'][] = $post_data;
+		}
+		wp_reset_postdata();
+	}
+    
+    wp_send_json( $response );
+}
